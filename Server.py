206cac117dde8927001260ebc491c8ef9acf7d62
yasmimmartns <yasmigm@hotmail.com>
2018-04-15 19:32:55 -0300

    Commit python

--- Server.py
+++ Server.py
@@ -31,28 +31,34 @@ while True:
 	try:
 		# Receives the request message from the client
 		message =  connectionSocket.recv(1024)
-		receive =  message
+		#print(message)
 		# Extract the path of the requested object from the message
 		# The path is the second part of HTTP header, identified by [1]
-		filename = message.split()[1]             
 
-		# Because the extracted path of the HTTP request includes 
-		# a character '\', we read the path from the second character 
-		f = open(filename[1:])
-		# Store the entire contenet of the requested file in a temporary buffer
-		outputdata = f.read()
-		# Send the HTTP response header line to the connection socket
-		output = "HTTP/1.1 200 OK\r\n\r\n" 
-		connectionSocket.send(output.encode('utf-8'))
  
-		# Send the content of the requested file to the connection socket
-		for i in range(0, len(outputdata)):  
-			connectionSocket.send(outputdata[i].encode('utf-8'))
-		output2 = "\r\n"
-		connectionSocket.send(output2.encode('utf-8'))
+		if(len(message) != 0):
+
+			filename = message.split()[1]   
+
+		        # Because the extracted path of the HTTP request includes 
+		        # a character '\', we read the path from the second character
+			f = open(filename[1:])
+			# Store the entire contenet of the requested file in a temporary buffer
+			outputdata = f.read()
+			# Send the HTTP response header line to the connection socket
+			output = "HTTP/1.1 200 OK\r\n\r\n" 
+			connectionSocket.send(output.encode('utf-8'))
+ 
+			# Send the content of the requested file to the connection socket
+			for i in range(0, len(outputdata)):  
+				connectionSocket.send(outputdata[i].encode('utf-8'))
+			crlf = "\r\n"
+			connectionSocket.send(crlf.encode('utf-8'))
+
+			print(message.decode('utf-8'))
 	
-		# Close the client connection socket
-		connectionSocket.close()
+			# Close the client connection socket
+			connectionSocket.close()
 
 	except IOError:
 		# Send HTTP response message for file not found
@@ -62,8 +68,6 @@ while True:
 		connectionSocket.send(outputHtml.encode('utf-8'))
 		# Close the client connection socket
 		connectionSocket.close()
-
-	print(receive) 
 
 serverSocket.close()  
 
